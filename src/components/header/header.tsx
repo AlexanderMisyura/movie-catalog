f24244eabed95af74fd5b1414c0d0cce567fb94b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-26 22:24:38 +0300

    feat: update header layout (#16)

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,30 @@
 import { UrlPath } from '@ts-enums';
+import type { User } from '@ts-interfaces';
 import { NavLink } from 'react-router';
 
 import logoIcon from '@/assets/icons/Logo.svg';
 import userIcon from '@/assets/icons/User.svg';
+import { DEFAULT_SEARCH_QUERY } from '@/constants';
 
 import styles from './header.module.css';
 
-export const Header: React.FC = () => {
+export const Header: React.FC<{ user: User }> = ({ user }) => {
   return (
     <header className={`wrapper ${styles.header}`}>
       <NavLink to={UrlPath.HOME} className={styles.logo}>
         <img src={logoIcon} alt="mooz logo" />
       </NavLink>
 
-      <input type="text" placeholder="Search..." defaultValue="Batman" />
+      <input
+        className={styles.search}
+        type="text"
+        placeholder="Search..."
+        defaultValue={DEFAULT_SEARCH_QUERY}
+      />
 
       <div className={styles.user}>
         <img src={userIcon} alt="user icon" />
-        Your Name
+        {user.name}
       </div>
     </header>
   );
---
+++ src/constants/user.ts
@@ -0,0 +1,3 @@
+export const TEMP_USER = {
+  name: 'Your Name',
+};
