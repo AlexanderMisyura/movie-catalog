26c3ec89312a4f0d09752b90268fe70f4d12f36a
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-26 22:14:02 +0300

    feat: update header layout

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export { DEFAULT_SEARCH_QUERY } from './catalog';
+export { TEMP_USER } from './user';
--- src/root-layout.tsx
+++ src/root-layout.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@components';
 import { Outlet } from 'react-router';
 
+import { TEMP_USER } from './constants';
+
 export const RootLayout: React.FC = () => {
   return (
     <>
-      <Header />
+      <Header user={TEMP_USER} />
 
       <main className="wrapper">
         <Outlet />
