b32781f962df9e7eb6de806f1f407aac0038c436
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-27 17:22:39 +0300

    feat: add ImageLoader component
    Display spinner while image is loading, placeholder - if image is not loaded

---
+++ src/components/image-loader/image-loader.tsx
@@ -0,0 +1,44 @@
+import { clsx } from 'clsx';
+import { useEffect, useState } from 'react';
+
+import placeholderIcon from '@/assets/icons/placeholder.svg';
+import spinnerIcon from '@/assets/icons/spinner.svg';
+
+import styles from './image-loader.module.css';
+
+interface ImageLoaderProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+}
+
+export const ImageLoader: React.FC<ImageLoaderProps> = ({
+  src,
+  alt,
+  className,
+  ...props
+}) => {
+  const [imageSrc, setImageSrc] = useState<string>(spinnerIcon);
+  const [styleClassName, setStyleClassName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+
+    img.onload = () => {
+      setImageSrc(src);
+    };
+
+    img.onerror = () => {
+      setImageSrc(placeholderIcon);
+      setStyleClassName(styles.placeholder);
+    };
+  }, [src]);
+
+  return (
+    <img
+      src={imageSrc}
+      className={clsx(className, styleClassName)}
+      alt={alt}
+      {...props}
+    />
+  );
+};
