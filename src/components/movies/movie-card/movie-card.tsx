871baf3f634fb7a2ec3249bcc5dbb9d077f14d1e
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-27 18:24:33 +0300

    Cards layout (#17)
    * feat: add ImageLoader component
    
    Display spinner while image is loading, placeholder - if image is not loaded
    
    * feat: update cards layout

--- src/components/movies/movie-card/movie-card.tsx
+++ src/components/movies/movie-card/movie-card.tsx
@@ -1,3 +1,4 @@
+import { ImageLoader } from '@components';
 import type { Movie } from '@ts-types';
 
 import styles from './movie-card.module.css';
@@ -5,7 +6,11 @@ import styles from './movie-card.module.css';
 export const MovieCard: React.FC<Movie> = (movie) => {
   return (
     <figure className={styles.card}>
-      <img className={styles.poster} src={movie.Poster} alt={movie.Title} />
+      <ImageLoader
+        className={styles.poster}
+        src={movie.Poster}
+        alt={movie.Title}
+      />
       <div className={styles.info}>
         <figcaption>Name: {movie.Title}</figcaption>
         <p>Year: {movie.Year}</p>
