5b96a4179ee1b8159f19b4af76a42c06de29f884
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-28 15:28:28 +0300

    feat: extract search-title component from catalog page

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { Header } from './header/header';
 export { ImageLoader } from './image-loader/image-loader';
 export { MovieCard } from './movies/movie-card/movie-card';
 export { MovieGrid } from './movies/movie-grid/movie-grid';
+export { SearchTitle } from './movies/search-title/search-title';
 export { Spinner } from './spinner/spinner';
