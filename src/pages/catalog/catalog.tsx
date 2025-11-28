776a5c1800bb2eb8198b8027c911b38e945fcf2e
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-28 15:35:55 +0300

    feat: add catalog movie fetch depending on url search params

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,96 +1,26 @@
-import { MovieGrid } from '@/components';
+import { ErrorBoundary, MovieGrid, SearchTitle, Spinner } from '@components';
+import { useMoviesPromise } from '@hooks';
+import { Suspense } from 'react';
 
 import styles from './catalog.module.css';
 
 export const Catalog = () => {
-  const searchTerm = 'Batman';
-
-  const tempMovies = [
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-    {
-      Title: 'Batman',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-      imdbID: 'tt0372784',
-      Type: 'movie',
-      Year: '2005',
-    },
-  ];
-
-  const tempResultsNumber = 338;
+  const moviesPromise = useMoviesPromise();
 
   return (
     <>
       <title>Movie Catalog</title>
 
-      <div className={styles.searchLine}>
-        <div className={styles.searchTitle}>
-          You searched for:{' '}
-          <span className={styles.searchTerm}>{searchTerm}</span>{' '}
+      <ErrorBoundary>
+        <div className={styles.catalogData}>
+          <Suspense fallback={<Spinner />}>
+            <SearchTitle moviesPromise={moviesPromise} />
+            <MovieGrid moviesPromise={moviesPromise} />
+          </Suspense>
         </div>
-        <span
-          className={styles.searchNumber}
-        >{`${tempResultsNumber} results`}</span>
-      </div>
 
-      <MovieGrid movies={tempMovies} />
-
-      <p>pagination</p>
+        <p>pagination</p>
+      </ErrorBoundary>
     </>
   );
 };
