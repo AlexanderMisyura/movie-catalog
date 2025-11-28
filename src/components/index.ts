e360ae05115a836aaab01c2bedce184b7f41ad8d
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-28 15:14:07 +0300

    feat: add error boundary component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { ErrorBoundary } from './error-boundary/error-boundary';
 export { Header } from './header/header';
 export { ImageLoader } from './image-loader/image-loader';
 export { MovieCard } from './movies/movie-card/movie-card';
