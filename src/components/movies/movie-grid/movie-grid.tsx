16f0cc34f8b8a85e2c3349cfc22daf66e60c3da0
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-11-29 15:29:55 +0300

    refactor: update movie components to more accurately display api search responses

--- src/components/movies/movie-grid/movie-grid.tsx
+++ src/components/movies/movie-grid/movie-grid.tsx
@@ -1,13 +1,28 @@
 import { MovieCard } from '@components';
-import type { MoviesData } from '@ts-interfaces';
+import { MovieErrorResponseSchema, MoviesDataSchema } from '@schemas';
+import type { MoviesData, MoviesErrorResponse } from '@ts-types';
 import { use } from 'react';
 
 import styles from './movie-grid.module.css';
 
-export const MovieGrid: React.FC<{ moviesPromise: Promise<MoviesData> }> = ({
-  moviesPromise,
-}) => {
-  const { movies, searchTerm } = use(moviesPromise);
+export const MovieGrid: React.FC<{
+  moviesPromise: Promise<MoviesData | MoviesErrorResponse>;
+}> = ({ moviesPromise }) => {
+  const resolved = use(moviesPromise);
+
+  const errorParsed = MovieErrorResponseSchema.safeParse(resolved);
+
+  if (errorParsed.success) {
+    return <p className={styles.message}>{errorParsed.data.Error}</p>;
+  }
+
+  const moviesDataParsed = MoviesDataSchema.safeParse(resolved);
+
+  if (!moviesDataParsed.success) {
+    return <p className={styles.message}>{moviesDataParsed.error.message}</p>;
+  }
+
+  const { movies, searchTerm } = moviesDataParsed.data;
 
   if (!searchTerm) {
     return (
@@ -15,10 +30,6 @@ export const MovieGrid: React.FC<{ moviesPromise: Promise<MoviesData> }> = ({
         To search for a movie, enter the name in the search field
       </p>
     );
-  }
-
-  if (!movies.length) {
-    return <p className={styles.message}>No movies found</p>;
   }
 
   return (
