cbb1677d034caacd0802e829668ff49d15f0feb9
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-12-06 19:25:53 +0300

    fix: update api config (#27)

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'http://www.omdbapi.com',
+  BASE_URL: 'https://www.omdbapi.com',
   API_KEY: import.meta.env.VITE_API_KEY,
   ITEMS_PER_PAGE: 10,
 } as const;
